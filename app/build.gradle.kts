9721fdcea4726847154ff83a83567e0a50085d0c
Sơn Tít <sonkoii1995@gmail.com>
2026-02-28 01:02:12 +0700

    Release v1.0.5_5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ls.petfunny"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
